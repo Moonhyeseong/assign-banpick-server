f704e263daf02f075356338b2b8a15e1e52a7b6d
Moonhyeseong <moonhs97@gmail.com>
2022-08-18 18:54:29 +0900

    Add: 밴픽 현황 DB 저장 완료

--- router/banpick.js
+++ router/banpick.js
@@ -4,14 +4,27 @@ const router = express.Router();
 //선택 버튼 클릭시
 router.post('/banpick/:id', (req, res) => {
   const filterID = req.params.id;
+  const { banList, pickList } = req.body.banPickList;
+
   const update = {
-    blue_pickList: req.body.blue_pickList,
-    red_pickList: req.body.red_pickList,
-    blue_banList: req.body.blue_banList,
-    red_banList: req.body.red_banList,
+    blue_pickList: pickList.blue,
+    red_pickList: pickList.red,
+    blue_banList: banList.blue,
+    red_banList: banList.red,
   };
 
-  InGame.findOneAndUpdate(filterID, update);
+  InGame.findOneAndUpdate({ _id: filterID }, update, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+router.get('/banpick/:id', (req, res) => {
+  const filterID = req.params.id;
+
+  InGame.findById(filterID, (err, result) => {
+    res.json(result);
+  });
 });
 
 module.exports = router;
