4b7f67fa764b1beedae72ce9de71ff8f93439276
Moonhyeseong <moonhs97@gmail.com>
2022-09-05 19:10:51 +0900

    DB 개선 브랜치

---
+++ models/gameData.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.ObjectId;
+
+const gameSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+
+  teamNames: {
+    type: Object,
+    required: true,
+    default: { blue: 'BLUE TEAM', red: 'RED TEAM' },
+  },
+
+  mode: {
+    type: Number,
+    required: true,
+  },
+
+  password: {
+    type: string,
+  },
+
+  banPickList: {
+    type: Object,
+    required: true,
+    default: {
+      banList: { blue: ['', '', '', '', ''], red: ['', '', '', '', ''] },
+      pickList: { blue: ['', '', '', '', ''], red: ['', '', '', '', ''] },
+    },
+  },
+
+  userList: {
+    type: Object,
+    required: true,
+  },
+
+  editer: {
+    type: Object,
+  },
+
+  isProceeding: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+    expires: 3600 * 24 * 3,
+  },
+});
+
+module.exports = mongoose.model('gameData', gameSchema);
